8afeed321141a97d69b0f0d3f604c1c527409fff
AsavaAsava <wayne.majani@hotmail.com>
2022-06-22 13:59:01 +0300

    Complete

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+
+
+var inp = document.getElementById('myInput');
+var p = document.getElementById('myP');
+
+function displayName(){
+    p.textContent = "Hello " + inp.value  + ", welcome to the bootcamp.";
+}
+
